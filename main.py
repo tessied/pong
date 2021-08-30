46349a4e1b3fd9250a7d87d7c6588c2715ac00eb
tessied <tessied@uchicago.edu>
2021-08-30 16:39:40 -0400

    Add paddle class to create two paddles to move with corresponding keys

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 
 screen.title("Pong")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+
+game_over = False
+while not game_over:
+    screen.update()
+    screen.onkey(r_paddle.move_up, "Up")
+    screen.onkey(r_paddle.move_down, "Down")
+    screen.onkey(l_paddle.move_up, "w")
+    screen.onkey(l_paddle.move_down, "s")
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.pu()
+        self.color("white")
+        self.goto(position)
+
+    def move_up(self):
+        self.sety(self.ycor() + 20)
+
+    def move_down(self):
+        self.sety(self.ycor() - 20)
+
